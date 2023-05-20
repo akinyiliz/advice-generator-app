6645b7396aeb4e6f1c33048ab9576db44895ca63
akinyiliz <lizzieke84@gmail.com>
2023-05-20 15:29:19 +0300

    Created and styled the structre of the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
+import "./App.css";
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Advice Generator App</h1>
+		<div className="app">
+			<div className="advice-container">
+				<p className="advice-number">Advice #11</p>
+
+				<p className="quote">
+					"Words can be like X-rays, if you use them properly—they’ll go through
+					anything. You read and you’re pierced."
+				</p>
+
+				<img
+					src="/images/pattern-divider-desktop.svg"
+					alt="Pattern divider"
+					className="pattern-divider"
+				/>
+				<div className="icon-dice-container">
+					<img
+						src="/images/icon-dice.svg"
+						alt="Dice"
+						className="icon-dice"
+					/>
+				</div>
+			</div>
 		</div>
 	);
 }
